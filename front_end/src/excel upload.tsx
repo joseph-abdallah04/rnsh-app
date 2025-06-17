42ec801d95aa594b4a54c4bfe34336766a56c486
Asmi Shrestha <asmishrestha151@gmail.com>
2025-06-17 16:23:06 +1000

    Created Nav_bar and Excel_upload page
    Created nav_bar and Excel upload page but is not finished. Need to add correct icons and centre everything with formatting as per the figma

--- front_end/src/excel upload.tsx
+++ front_end/src/excel upload.tsx
@@ -3,30 +3,32 @@ import './excel upload.css'
 
 function App() {
   return (
-    <div className="cover-bg">
+    <div className="excel-bg">
       <NavBar />
-      <div className="cover-content">
-        <div className="upload-box">
-          <div className="upload-area">
-            <img src="/upload-icon.svg" alt="Upload Icon" className="upload-icon" />
-            <p>Drag and drop file or<br /> click to upload</p>
+      <div className="excel-main">
+        <div className="excel-content">
+          <div className="excel-upload-box">
+            <img src="/upload-icon.svg" alt="Upload Icon" className="excel-upload-icon" />
+            <p className="excel-upload-title">Upload Icon</p>
+            <p className="excel-upload-desc">Drag and drop file or<br />click to upload</p>
+          </div>
+          <div className="excel-instructions">
+            <h2>Upload Instructions for data file</h2>
+            <p>Insert upload instructions here. Follow these instructions:</p>
+            <ol>
+              <li>Instruction one do this</li>
+              <li>Instruction two do that</li>
+              <li>Instruction three do this</li>
+            </ol>
           </div>
         </div>
-        <div className="instructions">
-          <h2>Upload Instructions for data file</h2>
-          <p>Insert upload instructions here. Follow these instructions:</p>
-          <ol>
-            <li>Instruction one do this</li>
-            <li>Instruction two do that</li>
-            <li>Instruction three do this</li>
-          </ol>
+        <div className="excel-actions">
+          <button className="excel-remove-btn">Remove File 🗑️</button>
+          <button className="excel-analyse-btn">Analyse ▶️</button>
         </div>
-      </div>
-      <div className="cover-actions">
-        <button className="remove-btn">Remove File 🗑️</button>
-        <button className="analyse-btn">Analyse ▶️</button>
       </div>
     </div>
   )
 }
+
 export default App
