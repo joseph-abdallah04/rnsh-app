0561a62ac959ee727d5999fb6c63264137ca9c92
Asmi Shrestha <asmishrestha151@gmail.com>
2025-06-18 10:14:14 +1000

    evenly divide the 3 titles

--- front_end/src/components/nav_bar.tsx
+++ front_end/src/components/nav_bar.tsx
@@ -3,13 +3,16 @@ import React from 'react'
 function NavBar() {
   return (
     <div className="navbar">
-      <button className="nav-btn active">📄 Excel Upload</button>
-      <button className="nav-btn">🎬 Video Upload</button>
-      <button className="nav-btn">💬 FAQs</button>
-      <span className="settings-icon">⚙️</span>
+      <div className="navbar-items"> 
+        <button className="nav-btn active">Excel Upload</button> 
+        <button className="nav-btn">Video Upload</button>
+        <span className="nav-btn">FAQs</span>
+        <span className="settings-icon">⚙️</span>
+      </div>
     </div>
   )
 }
 
 export default NavBar
+/* Line 7- active= make title blue */ 
 
