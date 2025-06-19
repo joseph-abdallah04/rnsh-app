708f1d69ba5bc0e0b38d175ec8b88a7fb7e98e43
Asmi Shrestha <asmishrestha151@gmail.com>
2025-06-19 10:46:50 +1000

    remove upload icon text

--- front_end/src/excel_upload.tsx
+++ front_end/src/excel_upload.tsx
@@ -19,8 +19,7 @@ function App() {
       <div className="excel-center">
         <div className="excel-content">
           <div className="excel-upload-box">
-            <img src="/upload-icon.png" alt="Upload Icon" className="excel-upload-icon" />
-            <p className="excel-upload-title">Upload Icon</p>
+          
             <p className="excel-upload-desc">Drag and drop file or<br />click to upload</p>
             <input type="file" onChange={handleFileUpload} />
           </div>
